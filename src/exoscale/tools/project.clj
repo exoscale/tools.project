38c7e1b24d3d9b8057779bcda000ec945307ac5d
Max Penet <mpenet@s-exp.com>
2022-05-17 09:42:41 +0200

    add validation

--- src/exoscale/tools/project.clj
+++ src/exoscale/tools/project.clj
@@ -1,15 +1,15 @@
 (ns exoscale.tools.project
   (:refer-clojure :exclude [compile])
-  (:require [exoscale.tools.project.api :as api]
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.spec.alpha :as s]
+            [clojure.tools.deps.alpha.util.dir :as td]
+            [exoscale.lingo :as l]
+            [exoscale.tools.project.api :as api]
+            [exoscale.tools.project.api.deploy :as deploy]
             [exoscale.tools.project.api.jar :as jar]
             [exoscale.tools.project.api.java :as java]
-            [exoscale.tools.project.api.deploy :as deploy]
-            [clojure.tools.deps.alpha.util.dir :as td]
-            [clojure.spec.alpha :as s]
-            [clojure.edn :as edn]
-            [clojure.java.io :as io]
-            [clojure.java.shell :as shell]
-            [exoscale.lingo :as l]))
+            [exoscale.tools.project.tasks :as tasks]))
 
 (def default-opts
   #:exoscale.project{:file "deps.edn"
@@ -108,54 +108,10 @@
       into-opts
       deploy/remote))
 
-(defn run-shell
-  [args {:keys [dir env]}]
-  (let [res (apply shell/sh
-                   (concat ["sh" "-c"]
-                           args
-                           (cond-> []
-                             dir
-                             (conj :dir dir)
-                             env
-                             (conj :env env))))]
-    (when (pos? (:exit res))
-      (throw (ex-info "Command failed to run" (assoc res :cmd args))))
-    (println (:out res))
-    res))
-
-(defn run-task
-  [task opts]
-  (td/with-dir (td/as-canonical (io/file (:dir opts)))
-    ((requiring-resolve (symbol task)) opts)))
-
-(defn- run-cmds!
-  [{:as _task :keys [commands tasks]} opts]
-  (if commands
-    (run! #(run-shell % opts) commands)
-    (run! #(run-task % opts) tasks)))
-
 (defn task
   [opts]
-  ;; let's assume we have the full env here
-  (let [{:exoscale.project/keys [deps-file]} (into-opts opts)
-        {:as root-deps-edn
-         :exoscale.project/keys [tasks]
-         :keys [id]} (edn/read-string (slurp (td/canonicalize (io/file deps-file))))
-        task-id (keyword (:id opts))
-        task (get tasks task-id)
-        for-all (:for-all task)]
-
-    (when-not task
-      (println (format "Task '%s' not found" id))
-      (System/exit 1))
-
-    (println (format "Running task: '%s'" (name task-id)))
-
-    (if for-all
-      (run! #(run-cmds! (assoc task :args opts)
-                        {:dir %})
-            (get-in root-deps-edn for-all))
-      (run-cmds! task nil))))
-
+  (-> opts
+      into-opts
+      tasks/task))
 
 
