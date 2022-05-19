4761ecc1d0e47fcbbd940b2e976b6d25fe12bc59
Max Penet <mpenet@s-exp.com>
2022-05-19 20:58:19 +0200

    do not rely on clj.java.shell

--- src/exoscale/tools/project/io.clj
+++ src/exoscale/tools/project/io.clj
@@ -1,15 +1,26 @@
 (ns exoscale.tools.project.io
-  (:require [clojure.java.shell :as shell]))
+  (:require [clojure.java.io :as io]))
+
+(defn cmd!
+  [cmd {:keys [dir env]}]
+  (let [pb (ProcessBuilder. ^java.util.List cmd)]
+    (when dir
+      (.directory pb (io/file dir)))
+    (when env
+      (let [pb-env (.environment pb)]
+        (run! (fn [[k v]] (.put pb-env k v)) env)))
+    (let [proc (.start pb)
+          exit (.waitFor proc)
+          out (slurp (.getInputStream proc))
+          err (slurp (.getErrorStream proc))]
+      (cond-> {:exit exit}
+        out (assoc :out out)
+        err (assoc :err err)))))
 
 (defn shell
-  [cmds {:keys [dir env]}]
+  [cmds {:keys [_dir _env] :as opts}]
   (run! (fn [cmd]
-          (let [res (apply shell/sh
-                           (cond-> ["sh" "-c" cmd]
-                             dir
-                             (conj :dir dir)
-                             env
-                             (conj :env env)))]
+          (let [res (cmd! ["sh" "-c" cmd] opts)]
             (when (pos? (:exit res))
               (throw (ex-info "Command failed to run" (assoc res :cmd cmd))))
             (println (:out res))
