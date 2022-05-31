de068748493b6c0dfa0caac13ebff2417686ed48
Pierre-Yves Ritschard <pyr@spootnik.org>
2022-05-31 10:08:20 +0200

    use tools build api instead of babashka process

--- src/exoscale/tools/project/io.clj
+++ src/exoscale/tools/project/io.clj
@@ -1,13 +1,19 @@
 (ns exoscale.tools.project.io
-  (:require [babashka.process :as p]))
+  (:require [clojure.tools.build.tasks.process :as p]))
+
+(defn- check
+  [{:keys [exit] :as res}]
+  (when-not (zero? exit)
+    (throw (ex-info "non zero exit" {})))
+  res)
 
 (defn shell
   "Run shell commands contained in `cmds`. The first unsuccesful exit triggers a system exit."
   [cmds {:keys [dir env]}]
   (try
     (doseq [cmd cmds]
-      (-> (p/process ["sh" "-c" cmd] {:out :inherit :err :inherit :dir dir :env env})
-          (p/check)))
+      (-> (p/process {:command-args  ["sh" "-c" cmd] :dir dir :env env})
+          (check)))
     (catch Exception _
       ;; At this stage we already printed the relevant error to stdout
       (System/exit 1))))
